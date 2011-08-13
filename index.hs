6c8219e72893800ee4d893af64258d2e0c69744b
Fabrice Medio <fmedio@gmail.com>
2011-08-13 07:34:36 -0700

    Build term-doc matrix

--- index.hs
+++ index.hs
@@ -1,22 +1,19 @@
-module Index where
+module Index(tokenize, termDocMatrix) where
 
 import qualified Data.Char as Char
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Matrix                                          
 
-data ParserState = ParserState { tokens :: [[Char]] 
-                               , currentToken :: [Char]
-                               , input :: [Char]
-                               } deriving Show
-                                          
-_tokenize tokens@(_) currentToken@(_) input@("") =
+parse tokens@(_) currentToken@(_) "" =
   if length currentToken /= 0 then
-    ParserState (doctorToken currentToken : tokens) "" ""
+    doctorToken currentToken : tokens
   else
-    ParserState tokens "" "" 
+    tokens
     
-_tokenize tokens currentToken (input:inputs) =
+parse tokens currentToken (input:inputs) =
   let (moreTokens, moreCurrentToken) = filter tokens currentToken in
-  _tokenize moreTokens moreCurrentToken inputs
+  parse moreTokens moreCurrentToken inputs
   where 
     filter tokens currentToken = 
       if isPunctuation input then
@@ -27,12 +24,33 @@ _tokenize tokens currentToken (input:inputs) =
       else
         (tokens, input : currentToken)
           
-tokenize s = tokens $ _tokenize [] "" s   
+tokenize s = parse [] "" s   
 
-doctorToken token = map Char.toLower $ reverse token
+doctorToken token = reverse . map Char.toLower $ token
 
 isPunctuation char = Set.member char $ Set.fromList ['.', ',', ' ']
                
+indexOneDoc terms termVector [] = (terms, termVector)
+
+indexOneDoc terms termVector (token:tokens) = 
+  let count = Map.findWithDefault 0 token termVector in
+  indexOneDoc (Set.insert token terms) (Map.insert token (count + 1) termVector) tokens
+  
+indexAll terms termVectors [] = (terms, termVectors)
+
+indexAll terms termVectors (doc:docs) = 
+  let (terms', termVector) = indexOneDoc terms Map.empty $ tokenize doc in
+  indexAll terms' (termVector : termVectors) docs
+  
+termDocMatrix docs = 
+  let 
+    (termSet, termVectors) = indexAll Set.empty [] docs 
+    terms = Set.toList termSet
+    matrix = map (\ termVector -> map (\ term -> (Map.findWithDefault 0.0 term termVector)::Double ) terms) termVectors
+  in
+   (terms, matrix)
+  
 
 
-
+  
+  
--- index_test.hs
+++ index_test.hs
@@ -4,9 +4,9 @@ import Index
 import Test.HUnit
 
 tests = test [
-  "testTokenize" ~: "" ~: ["bar", "foo"] ~=? (tokenize "foo bar"),
-  "testTokenizePunctuation" ~: "" ~: ["panda", "bar", "foo"] ~=? (tokenize "foo, bar. panda"),
-  "testMixedCase" ~: "" ~: ["panda", "bar", "foo"] ~=? (tokenize "fOo, bar. pandA"),
-  "testTermDocMatrix" ~: "" ~: [[3,2],[1,4]] ~=? (termDocMatrix ["x y x y x", "y x y y y"])
+  "testTokenize" ~: "" ~: ["bar", "foo"] ~=? (tokenize "foo bar")
+  , "testTokenizePunctuation" ~: "" ~: ["panda", "bar", "foo"] ~=? (tokenize "foo, bar. panda")
+  , "testMixedCase" ~: "" ~: ["panda", "bar", "foo"] ~=? (tokenize "fOo, bar. pandA")
+  , "testTermDocMatrix" ~: "" ~: [[1,4],[3,2]] ~=? (termDocMatrix ["x y x y x", "y x y y y"])
   
              ]
