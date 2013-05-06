4a58ae852100b157cf433ab31ba0cefa3924514d
Fabrice Medio <fmedio@gmail.com>
2013-05-06 11:14:00 -0700

    [fm] Trie works

--- trie.hs
+++ trie.hs
@@ -3,19 +3,22 @@ data Trie a =
           , children :: [Trie a] } deriving Show
 
 
-sequenceContains :: (Eq a) => a -> [a] -> Bool
-sequenceContains a [] = False
-sequenceContains a (x:xs) = if (a == x) then True else sequenceContains a xs
+find :: (Eq a) => a -> [Trie a] -> Maybe (Trie a)
+find a [] = Nothing
+find a (t:ts) = if (Just a) == value t then (Just t) else find a ts
 
           
 insert :: (Eq a) => [a] -> Trie a -> Trie a
 insert [] t = t
 insert (x:xs) t = 
-       if sequenceContains (Just x) (map (\e -> value e) (children t)) then 
-       Trie (value t) $ map (\e -> if Just x == value e 
-                                   then insert xs e
-                                   else e) 
-                             $ children t
-       else 
-       Trie (value t) ((insert xs (Trie (Just x) [])) : (children t))
+       case find x (children t) of
+       Nothing   -> Trie (value t) ((insert xs (Trie (Just x) [])) : (children t))
+       _         -> Trie (value t) $ map (\e -> if Just x == value e 
+                                         then insert xs e
+                                         else e) 
+                                   $ children t
 
+contains :: (Eq a) => [a] -> Trie a -> Bool
+contains [] t = True
+contains (x:xs) t = let child = find x (children t) in
+         maybe False (\e -> contains xs e) child
